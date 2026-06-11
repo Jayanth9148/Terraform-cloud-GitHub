9c9178af42ef1dd31ce40dcc690f32d4c1d4265f
jayanth9148 <jayanth9148@gmail.com>
2026-06-11 17:16:14 +0530

    jayanth modified new tag name

--- main.tf
+++ main.tf
@@ -17,6 +17,6 @@ resource "aws_instance" "jayanthinfra" {
 	ami = "ami-00e801948462f718a" 
 	instance_type = "t3.micro"
 tags = {
-   Name = "jayanth_server"
+   Name = "ramachandra_server"
       }
 }
